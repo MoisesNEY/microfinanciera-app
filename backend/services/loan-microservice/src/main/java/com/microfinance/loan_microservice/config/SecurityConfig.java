01216bc749ab5a75e49b6c9052667a1a9f638c3d
eduar2agm <eduar2.agm2020@gmail.com>
2025-10-01 20:16:26 -0600

    feat(loan-microservice): actualizar integración con SpringDoc y seguridad

--- backend/services/loan-microservice/src/main/java/com/microfinance/loan_microservice/config/SecurityConfig.java
+++ backend/services/loan-microservice/src/main/java/com/microfinance/loan_microservice/config/SecurityConfig.java
@@ -1,34 +1,34 @@
-package com.microfinance.gateway;
+package com.microfinance.loan_microservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
   @Bean
-  SecurityWebFilterChain springSecurityFilterChain(
-      ServerHttpSecurity http,
-      @Value("${GATEWAY_SECURITY_DISABLED:false}") boolean disabled
-  ) {
-    http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+  SecurityFilterChain filterChain(
+      HttpSecurity http,
+      @Value("${APP_SECURITY_DISABLED:false}") boolean disabled
+  ) throws Exception {
+    http.csrf(csrf -> csrf.disable());
 
     if (disabled) {
       // Modo libre (primer arranque sin Keycloak)
       return http
-          .authorizeExchange(ex -> ex.anyExchange().permitAll())
+          .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
           .build();
     }
 
     // Modo seguro (con Keycloak)
     return http
-        .authorizeExchange(ex -> ex
-            .pathMatchers("/actuator/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-            .anyExchange().authenticated()
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/actuator/**","/v3/api-docs/**","/swagger-ui.html","/swagger-ui/**").permitAll()
+            .anyRequest().authenticated()
         )
         .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
         .build();
